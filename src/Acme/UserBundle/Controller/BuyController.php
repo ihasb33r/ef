8831ac3f76b5ddef87d9eb86239025436463a983
araksia <k.araksia@gmail.com>
2012-05-28 22:30:31 +0300

    buycontroller buytwigs kai routing.yml

--- src/Acme/UserBundle/Controller/BuyController.php
+++ src/Acme/UserBundle/Controller/BuyController.php
@@ -12,53 +12,42 @@ class BuyController extends Controller
 {
     public function indexAction($id)
     { 
-
 	 $em = $this->getDoctrine()->getEntityManager();
 		 $sell = $em->getRepository('AcmeUserBundle:Sell')->findAll();
-
-    return $this->render('AcmeUserBundle:Default:buy.html.twig', array ('sell'=>$sell, 'id'=>$id));
+	        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+		
+        $buy= new Buy();
+        $form =$this->createFormBuilder($buy)
+            ->add('quantity', 'number')
+            ->getForm();
+        if ($this->getRequest()->getMethod()==="POST"){
+		
+            $form->bindRequest($this->getRequest());
+            if ($form->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+				$quantity=$buy->getQuantity();
+                $buy->setUser($user);
+				$sell = $em->getRepository('AcmeUserBundle:Sell')->find($id);
+				$sell->setAgor($sell->getAgor()+$quantity);
+				$buy->setSell($sell);
+                $em->persist($buy);
+                $em->flush();
+            }
+			return $this->redirect($this->generateUrl('buy_new'));
+        }
+        return $this->render('AcmeUserBundle:Default:buy.html.twig', array("location_id"=>$id,"form"=>$form->createView(), "sell"=>$sell, "id"=>$id));
     }
+
+
+    
 
 
     public function newAction()
     {
-        echo 'aaaaaaaaa';
-        $id=$_POST['id'];
-        $quantity=$_POST['quant'];
-        $em = $this->getDoctrine()->getEntityManager();
-        $se = $em->getRepository('AcmeUserBundle:Sell')->findAll();
-        foreach ($se as $sell) {
-            if($sell->getId()==$id)
-            {$a=$sell->getAgor();
-            $q=$sell->getQuantity();}}
-            $upol=$q-$a;
-            $dia8=$upol-$quantity;
-            if ($dia8>=0)
-            {
-
-
-                $user = $this->container->get('security.context')->getToken()->getUser();
-                if (!is_object($user) || !$user instanceof UserInterface) {
-                    throw new AccessDeniedException('This user does not have access to this section.');
-                }
-                $idi=$user->getId();
-
-                $sell = $em->getRepository('AcmeUserBundle:Sell')->find($id);	
-                $sell->setAgor($sell->getAgor()+$quantity);
-                $buy= new Buy();
-                $buy->setQuantity($_POST['quant']);
-                $buy->setUser($user);
-                $buy->setSell($sell);
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($buy);
-                $em->persist($sell);
-                $em->persist($user);
-                $em->flush();
-
-                $kod[]=$buy->getId();}
-               // return $this->render('AcmeUserBundle:Default:buy_new.html.twig', array('kod'=>$kod));
-
-
+     return $this->render('AcmeUserBundle:Default:buy_new.html.twig'); 	   
     }
 
 
