d40860d106b2a14684468a5ea1ea777b99ddf064
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-05-29 01:12:21 +0300

    add firewall and routing rules

--- src/Acme/UserBundle/Controller/BuyController.php
+++ src/Acme/UserBundle/Controller/BuyController.php
@@ -12,42 +12,48 @@ class BuyController extends Controller
 {
     public function indexAction($id)
     { 
-	 $em = $this->getDoctrine()->getEntityManager();
-		 $sell = $em->getRepository('AcmeUserBundle:Sell')->findAll();
-	        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-		
+
         $buy= new Buy();
         $form =$this->createFormBuilder($buy)
             ->add('quantity', 'number')
             ->getForm();
-        if ($this->getRequest()->getMethod()==="POST"){
-		
-            $form->bindRequest($this->getRequest());
-            if ($form->isValid()){
-                $em = $this->getDoctrine()->getEntityManager();
-				$quantity=$buy->getQuantity();
-                $buy->setUser($user);
-				$sell = $em->getRepository('AcmeUserBundle:Sell')->find($id);
-				$sell->setAgor($sell->getAgor()+$quantity);
-				$buy->setSell($sell);
-                $em->persist($buy);
-                $em->flush();
-            }
-			return $this->redirect($this->generateUrl('buy_new'));
-        }
+        $em = $this->getDoctrine()->getEntityManager();
+        $current_location = $em->getRepository("AcmeUserBundle:Location")->find($id);
+        $sell = $em->getRepository('AcmeUserBundle:Sell')->findBy(array("location"=>$id));
         return $this->render('AcmeUserBundle:Default:buy.html.twig', array("location_id"=>$id,"form"=>$form->createView(), "sell"=>$sell, "id"=>$id));
     }
 
 
-    
-
-
-    public function newAction()
+    public function newAction( $location_id, $offer_id)
     {
-     return $this->render('AcmeUserBundle:Default:buy_new.html.twig'); 	   
+        $buy= new Buy();
+        $form =$this->createFormBuilder($buy)
+            ->add('quantity', 'number')
+            ->getForm();
+
+        if ($this->getRequest()->getMethod()==="POST"){
+
+            $form->bindRequest($this->getRequest());
+            if ($form->isValid()){
+                $user = $this->container->get('security.context')->getToken()->getUser();
+                if (!is_object($user) || !$user instanceof UserInterface) {
+                    throw new AccessDeniedException('This user does not have access to this section.');
+                }
+                $em = $this->getDoctrine()->getEntityManager();
+                $quantity=$buy->getQuantity();
+                $buy->setUser($user);
+                $sell = $em->getRepository('AcmeUserBundle:Sell')->find($offer_id);
+                $sell->setAgor($sell->getAgor()+$quantity);
+                $buy->setSell($sell);
+                $em->persist($buy);
+                $em->flush();
+            }
+            return $this->render('AcmeUserBundle:Default:buy_new.html.twig'); 	   
+        }
+        return $this->redirect($this->generateUrl('buy'));
+
+
+
     }
 
 
