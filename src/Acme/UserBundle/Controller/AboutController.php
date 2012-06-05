966347a4277d6e383e7c6fc96721db7522d45024
araksia <k.araksia@gmail.com>
2012-06-05 11:59:34 +0300

    price kai about allaksa

---
+++ src/Acme/UserBundle/Controller/AboutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Acme\UserBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AboutController extends Controller
+{
+ 
+    public function newAction()
+    {
+      
+			 
+        return $this->container->get('templating')->renderResponse('AcmeUserBundle:Default:about.html.twig');
+    }
+
+  }
