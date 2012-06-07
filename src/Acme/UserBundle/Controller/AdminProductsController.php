eb16ff7c8a40ce786a8883649209a345d22daba7
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-07 19:28:23 +0300

    added location management options

--- src/Acme/UserBundle/Controller/AdminProductsController.php
+++ src/Acme/UserBundle/Controller/AdminProductsController.php
@@ -31,54 +31,22 @@ class AdminProductsController extends Controller
         $product = new Product();
         $form = $this->createFormBuilder($product)
             ->add('name', "text")
+            ->add('category', "text")
             ->add('file', "file")
             ->getForm()
             ;
-        if ($this->getRequest()->get('ajax')=="true"){
-            if ($this->getRequest()->getMethod() === 'POST') {
-                $form->bindRequest($this->getRequest());
-                if ($form->isValid()){
-                    $em = $this->getDoctrine()->getEntityManager();
-                    $product->upload();
-                    $em->persist($product);
-                    $em->flush();
-                }
+        if ($this->getRequest()->getMethod() === 'POST') {
+            $form->bindRequest($this->getRequest());
+            if ($form->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+                $product->upload();
+                $em->persist($product);
+                $em->flush();
             }
-            $em = $this->getDoctrine()->getEntityManager();
-            $products = $em->getRepository('AcmeUserBundle:Product')->findAll();
         }
+        $em = $this->getDoctrine()->getEntityManager();
+        $products = $em->getRepository('AcmeUserBundle:Product')->findAll();
         return $this->render('AcmeUserBundle:Admin:add_product.html.twig', array('form'=>$form->createView()));
-
-
-
-        /*
-        $id=$_POST['id'];	
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        if (!is_object($user) || !$user instanceof UserInterface) {
-            throw new AccessDeniedException('This user does not have access to this section.');
-        }
-        $idi=$user->getId();
-        $em = $this->getDoctrine()->getEntityManager();
-        $location = $em->getRepository('AcmeUserBundle:Location')->find($id);
-
-        $sell= new Sell();
-        $sell->setQuantity($_POST['quantity']);
-        $sell->setMinquantity($_POST['min_quantity']);
-        $sell->setPrice($_POST['price']);
-        $sell->setOrigin($_POST['origin']);
-        $sell->setAgor($_POST['agor']);
-        $sell->setUser($user);
-        $sell->setLocation($location);
-        $em = $this->getDoctrine()->getEntityManager();
-        $em->persist($location);
-        $em->persist($sell);
-        $em->persist($user);
-        $em->flush();
-        return $this->render('AcmeUserBundle:Default:sell_new.html.twig');
-         */
     }
-
-
-
 
 }
