f0b9eb721e2639f37d9320a220cbb594d021ba40
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-08 15:30:39 +0300

    fix organiser approval query

--- src/Acme/UserBundle/Controller/ApprovalController.php
+++ src/Acme/UserBundle/Controller/ApprovalController.php
@@ -11,12 +11,12 @@ class ApprovalController extends Controller
 {
     public function indexAction(Request $request)
     {
-         
+
         $user = $this->container->get('security.context')->getToken()->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-		$id=$user->getId();
+        $id=$user->getId();
 
 
         $em = $this->getDoctrine()->getEntityManager();
@@ -24,35 +24,48 @@ class ApprovalController extends Controller
         $locat =  $qb
             ->innerJoin("p.location", "l")
             ->where($qb->expr()->andX(
+                $qb->expr()->eq('l.approved',':approval'),
                 $qb->expr()->gt('l.date',':date'),
-                        $qb->expr()->eq('l.user',':user')))
-                ->setParameter('date',new \DateTime("today"))
-                ->setParameter('user', $user)
+                $qb->expr()->eq('l.user',":user")
+                ))
+                ->setParameter('approval', "0")
+                ->setParameter('date', new \Datetime("today"))
+                ->setParameter('user', $user->getId())
                 ->orderby('l.date', 'ASC')
                 ->getQuery()
                 ->getResult();
+        /*
+
+        $locat =  $qb
+            ->innerJoin("p.location", "l")
+            ->innerJoin("l.user", "u")
+            ->where($qb->expr()->eq('u.id',':user')
+                )
+                ->setParameter("user", $user->getId())
+                ->orderby('l.date', 'ASC')
+                ->getQuery()
+                ->getResult();
+         */
 
 
-
-
-//        $em = $this->getDoctrine()->getEntityManager();
-//		$location = $em->getRepository("AcmeUserBundle:Location")->findAll();
-//		$sell = $em->getRepository("AcmeUserBundle:Sell")->findAll();
+        //        $em = $this->getDoctrine()->getEntityManager();
+        //		$location = $em->getRepository("AcmeUserBundle:Location")->findAll();
+        //		$sell = $em->getRepository("AcmeUserBundle:Sell")->findAll();
         return $this->render('AcmeUserBundle:Organiser:approval.html.twig', array('items'=>$locat, 'id'=>$id));
     }
     public function okAction($id)
     { 
-	     $em = $this->getDoctrine()->getEntityManager();
-		 $sell = $em->getRepository('AcmeUserBundle:Sell')->findById($id);
-		 echo $sell->getId();
-		 $sell->setApproved(true);
-		 $em->flush();
-		
-      return $this->indexAction();
+        $em = $this->getDoctrine()->getEntityManager();
+        $sell = $em->getRepository('AcmeUserBundle:Sell')->findById($id);
+        echo $sell->getId();
+        $sell->setApproved(true);
+        $em->flush();
+
+        return $this->indexAction();
     }
-	 public function noAction($id)
+    public function noAction($id)
     {
-        
+
         $em = $this->getDoctrine()->getEntityManager();
         $sell= $em->getRepository('AcmeUserBundle:Sell')->findOneById($id);
         $em->remove($sell);
