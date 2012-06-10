35df54ed842ce6aaaaca52ec2d960c432858e317
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-10 17:36:02 +0300

    twig work

--- src/Acme/UserBundle/Controller/LocationsController.php
+++ src/Acme/UserBundle/Controller/LocationsController.php
@@ -11,7 +11,7 @@ class LocationsController extends Controller
 {
     public function indexAction(Request $request)
     {
-         $location = new Location();
+        $location = new Location();
         $form = $this->createFormBuilder($location)
             ->add('name', "text")
             ->add('longitude', "number")
@@ -20,34 +20,33 @@ class LocationsController extends Controller
             ->add('extrainfo', "textarea")
             ->add('date', "date")
             ->add('address', "text")
-			->add('phone', "number")
-			->add('starttime', "time")
-			->add('endtime', "time")
+            ->add('phone', "number")
+            ->add('starttime', "time")
+            ->add('endtime', "time")
             ->add('product', "entity", array('class'=>'AcmeUserBundle:Product', 'property'=>'name') )
             ->getForm()
             ;
-			$user = $this->container->get('security.context')->getToken()->getUser();
+        $user = $this->container->get('security.context')->getToken()->getUser();
         if (!is_object($user) || !$user instanceof UserInterface) {
             throw new AccessDeniedException('This user does not have access to this section.');
         }
-        if ($this->getRequest()->get('ajax')=="true"){ 
-            if ($this->getRequest()->getMethod() === 'POST') {
-                $form->bindRequest($this->getRequest());
-                if ($form->isValid()){
-                    $em = $this->getDoctrine()->getEntityManager();
-					$location->setApproved(false);
-					$location->setPublic(false);
-					$location->setUser($user);
-                    $em->persist($location);
-                    $em->flush();
-                }return $this->redirect($this->generateUrl('locations_new'));
+        if ($this->getRequest()->getMethod() === 'POST') {
+            $form->bindRequest($this->getRequest());
+            if ($form->isValid()){
+                $em = $this->getDoctrine()->getEntityManager();
+                $location->setApproved(false);
+                $location->setPublic(false);
+                $location->setUser($user);
+                $em->persist($location);
+                $em->flush();
             }
+            return $this->redirect($this->generateUrl('locations_new'));
         }
         return $this->render('AcmeUserBundle:Organiser:locations.html.twig', array('form'=>$form->createView()));
     }
     public function newAction()
     {
-        
+
         return $this->render('AcmeUserBundle:Organiser:add_location.html.twig');
     }
 }
