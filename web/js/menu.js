18b6b3998dacd545eb6e55a296dd8e3f8d00a35d
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-10 18:11:55 +0300

    fix menu.js to ingore empty menu entries

--- web/js/menu.js
+++ web/js/menu.js
@@ -2,9 +2,15 @@ $(document).ready( function (){
     var optgroups = $("optgroup");
     $.each(optgroups, function(key, opt){
         var links = $("#" + $(opt).attr("class")+" a");
+        if (links.length<1){
+            console.log(opt);
+            $(opt).remove();
+        }
+        else{
         $.each(links, function( key, link){
             $(opt).append(" <option value='" + $(link).attr("href") +"'>"+$(link).text()+ "</option>");
         });
+        }
     });
 
     $("#navigation").change(function(){
