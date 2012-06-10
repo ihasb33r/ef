4161cb8ac512d6f3562fe2bf690240ec0d09df55
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-10 22:20:28 +0300

    fix newsletter register

--- src/Acme/UserBundle/Controller/NewsletterController.php
+++ src/Acme/UserBundle/Controller/NewsletterController.php
@@ -9,62 +9,63 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerAware;
 class NewsletterController extends Controller
 {
-public function indexAction(Request $request)
-{echo"why?";  
- if ($this->getRequest()->getMethod() === 'POST') {
-          
-    $email = $request->request->get('email');
+    public function indexAction()
+    {
 
-    try {
+        $request = $this->getRequest();
+        if ($this->getRequest()->getMethod() === 'POST') {
 
-        $email = $request->request->get('email');
-        $em = $this->getDoctrine()->getEntityManager();
-        $query = $em->createQuery(
-            'SELECT u FROM AcmeUserBundle:User u WHERE u.email = :email'
-        )->setParameter('email', $email);
+            $email = $request->request->get('email');
 
-        $user = $query->getSingleResult();
+            try {
 
-        if(!is_object($user)){ //this means anonymous user not registered to site
-            $em = $this->getDoctrine()->getEntityManager();
-            $query = $em->createQuery(
-                'SELECT n FROM AcmeUserBundle:Newsletter n WHERE n.email = :email AND n.isSubscribed = 1'
-            )->setParameter('email', $email);
-
-            $record = $query->getSingleResult();
-
-            if($record){
-                $msg = "You are already subscribed!";
-            }else{
-                $newsletter = new Newsletter();
-                $newsletter->setEmail($email);
-                $newsletter->setIsSubscribed(true);
-
+                $email = $request->request->get('email');
                 $em = $this->getDoctrine()->getEntityManager();
-                $em->persist($newsletter);
-                $em->flush();
+                $query = $em->createQuery(
+                    'SELECT u FROM AcmeUserBundle:User u WHERE u.email = :email'
+                )->setParameter('email', $email);
 
-                $msg = "You have been subscribed to our newsletter! Thank You.";
+                $user = $query->getSingleResult();
+
+                if(!is_object($user)){ //this means anonymous user not registered to site
+                    $em = $this->getDoctrine()->getEntityManager();
+                    $query = $em->createQuery(
+                        'SELECT n FROM AcmeUserBundle:Newsletter n WHERE n.email = :email AND n.isSubscribed = 1'
+                    )->setParameter('email', $email);
+
+                    $record = $query->getSingleResult();
+
+                    if($record){
+                        $msg = "You are already subscribed!";
+                    }else{
+                        $newsletter = new Newsletter();
+                        $newsletter->setEmail($email);
+                        $newsletter->setIsSubscribed(true);
+                        $em = $this->getDoctrine()->getEntityManager();
+                        $em->persist($newsletter);
+                        $em->flush();
+                        $msg = "You have been subscribed to our newsletter! Thank You.";
+                    }
+                }else{
+                    if($user->getNewsletterSubscription()){
+                        $msg = "You are already subscribed!";
+                    }else{
+                        $user->setNewsletterSubscription(1);
+                        $em = $this->getDoctrine()->getEntityManager();
+                        $em->flush();                    
+                        $msg = "You have been subscribed to our newsletter! Thank You.";
+                    }                
+                }
+
             }
-        }else{
-            if($user->getNewsletterSubscription()){
-                $msg = "You are already subscribed!";
-            }else{
-                $user->setNewsletterSubscription(1);
-                $em = $this->getDoctrine()->getEntityManager();
-                $em->flush();                    
-                $msg = "You have been subscribed to our newsletter! Thank You.";
-            }                
-        }
+            catch (\Exception $e) {
+                $msg = 'Ξ•Ο…Ο‡Ξ±ΟΞΉΟƒΟ„ΞΏΟΞΌΞµ Ξ³ΞΉΞ± Ο„ΞΏ ΞµΞ½Ξ΄ΞΉΞ±Ο†Ξ­ΟΟ‰Ξ½ ΟƒΞ±Ο‚';
+            }
 
+            if ($this->getRequest()->isXmlHttpRequest()) {
+                return new \Symfony\Component\HttpFoundation\Response($msg);
+                return array('msg' => $msg);
+            } return $this->render('AcmeUserBundle:Default:newslatter.html.twig', array('msg' => $msg));} 
+                return $this->render('AcmeUserBundle:Default:base.html.twig');
     }
-    catch (\Exception $e) {
-        $msg = 'Υπήρξε κάποιο πρόβλημα. Προσπαθείστε αργότερα';
-    }
-
-    if ($this->getRequest()->isXmlHttpRequest()) {
-        return new \Symfony\Component\HttpFoundation\Response($msg);
-        return array('msg' => $msg);
-    } return $this->render('AcmeUserBundle:Default:newslatter.html.twig', array('msg' => $msg));} 
-	return $this->render('AcmeUserBundle:Default:base.html.twig');
-}}
+}
