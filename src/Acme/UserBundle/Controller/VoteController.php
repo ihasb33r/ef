95e762e7d29f67df746a1c43d4eb827fd438a30d
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-12 19:14:50 +0300

    vote changes

--- src/Acme/UserBundle/Controller/VoteController.php
+++ src/Acme/UserBundle/Controller/VoteController.php
@@ -2,7 +2,7 @@
 
 namespace Acme\UserBundle\Controller;
 use Acme\UserBundle\Entity\Buy;
-use Acme\UserBundle\Entity\Rating;
+use Acme\UserBundle\Entity\Rate;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -22,22 +22,30 @@ class VoteController extends Controller
                 ->getQuery()
                 ->getResult();
         $votescasted = $em->getRepository('AcmeUserBundle:Rate')->findByUser($user->getId());
-/*
-        $user_id = $user->getId();
-        $votes_remaining =$em->getRepository('AcmeUserBundle:Buy')->findByUser($user_id);
-        $user = $this->container->get('security.context')->getToken()->getUser();
-        $em = $this->getDoctrine()->getEntityManager();
-        $votes = $em->getRepository('AcmeUserBundle:Rate')->findByCandidate($id);
- */
         return $this->render('AcmeUserBundle:Votes:uservotes.html.twig', array('votesremaining'=>$notvotedyet, 'votescasted'=>$votescasted));
     }
 
 
-    public function voteAction($canditate, $vote )
+    public function voteAction($candidate)
     {
-        $user = $this->container->get('security.context')->getToken()->getUser();
         $em = $this->getDoctrine()->getEntityManager();
-        $location = $em->getRepository('AcmeUserBundle:Rate')->findOneByUser($id);
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        $buy = $em->getRepository('AcmeUserBundle:Buy')->find($candidate);
+        $vote = $this->getRequest()->get("vote");
+        $comment = $this->getRequest()->get("comment");
+        
+        $rating=new Rate();
+        $rating->setUser($user);
+        $rating->setVath(intval($vote));
+        $rating->setSxolia($comment);
+        $em->persist($rating);
+        $em->flush();
+        $buy->setVote($rating);
+        $em->persist($buy);
+        $em->flush();
+        
+        return $this->redirect($this->generateUrl('givenvotes'));
+        
 
     }
 
