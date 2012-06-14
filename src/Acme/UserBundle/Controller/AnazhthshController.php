cf2a389a2249f5557a101ef1a2c2a61cbb765956
Kostas Papakonstantinou <bckdotgr@gmail.com>
2012-06-14 15:39:10 +0300

    fix searching

--- src/Acme/UserBundle/Controller/AnazhthshController.php
+++ src/Acme/UserBundle/Controller/AnazhthshController.php
@@ -3,6 +3,7 @@ namespace Acme\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Acme\UserBundle\Entity\Location;
+use Acme\UserBundle\Entity\Product;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -12,16 +13,40 @@ class AnazhthshController extends Controller
     public function indexAction()
     { 
 			 $em = $this->getDoctrine()->getEntityManager();
+             $products =$em->getRepository("AcmeUserBundle:Product")->findAll();
 			 $qb = $em->getRepository('AcmeUserBundle:Location')->createQueryBuilder("p");
             $locat =  $qb
-            ->innerJoin("p.product", "c")
-            ->where($qb->expr()->gt('p.date',':date'))
-            ->setParameter('date',new \DateTime("today"))
-			->orderby('p.date', 'ASC')
-            ->getQuery()
-            ->getResult();
+                ->select("p.town")
+                ->distinct()
+                ->where("p.town<>'' ")
+                ->getQuery()
+                ->getResult();
 
-      return $this->render('AcmeUserBundle:User:anazhthsh.html.twig', array('locat'=>$locat));  
+             if ($this->getRequest()->get("search")=="search"){
+
+                 $town = $this->getRequest()->get("town");
+                 $product = $this->getRequest()->get("product");
+                 $qb = $em->getRepository('AcmeUserBundle:Location')->createQueryBuilder("p");
+                 $qb
+                ->where('p.date>=:date')
+                ->setParameter('date',new \DateTime("today"));
+
+                 if (!empty($town)){
+                     $qb->andwhere('p.town like :town')
+                         ->setParameter('town', $town);
+                 }
+                 if (!empty($product)){
+                     $qb->andwhere('p.product= :product')
+                         ->setParameter('product', $em->getRepository('AcmeUserBundle:Product')->findOneById($product));
+                 }
+
+                 $results = $qb->orderby('p.date', 'ASC')
+                ->getQuery()->getResult();
+              return $this->render('AcmeUserBundle:User:anazhthsh.html.twig', array('locat'=>$locat, "products"=>$products, "results"=>$results));  
+
+             }
+
+      return $this->render('AcmeUserBundle:User:anazhthsh.html.twig', array('locat'=>$locat, "products"=>$products));  
     }
     public function newAction()
     {
